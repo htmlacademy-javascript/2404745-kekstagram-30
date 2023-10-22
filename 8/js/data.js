08dce014efac404e68e9e32bf86716afb88cd006
Keks <keks@htmlacademy.ru>
2023-10-22 11:26:56 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/data.js
+++ 8/js/data.js
@@ -1,29 +1,18 @@
 import {getRandomInteger, getUniqueRandomInteger, getRandomArrayElement} from './util.js';
-
-const MIN_COMMENTS = 0;
-const MAX_COMMENTS = 30;
-const PHOTOS_COUNT = 25;
-const MIN_LIKES = 15;
-const MAX_LIKES = 200;
-
-const MESSAGE = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-const NAME = [ 'Николай', 'Елена', 'Василий', 'Иван', 'Анна', 'Андрей' ];
-
-const DESCRIPTION = ['Городская площадь', 'На пляже', 'Городская ратуша', 'Статуя в парке', 'В лесу', 'Празднование дня рождения', 'В порту' ];
-
+import {
+  MIN_COMMENTS,
+  MAX_COMMENTS,
+  PHOTOS_COUNT,
+  MIN_LIKES,
+  MAX_LIKES,
+  MESSAGE,
+  NAME,
+  DESCRIPTION
+} from './constants.js';
 
 const getPhotoId = getUniqueRandomInteger(1, PHOTOS_COUNT);
 const getPhotoUrl = getUniqueRandomInteger(1, PHOTOS_COUNT);
 const getCommentId = getUniqueRandomInteger(0, 500);
-
 
 const createComment = () => ({
   id: getCommentId(),
